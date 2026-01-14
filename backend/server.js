fb9279504d7a2e492842d0c13eaa07c6a491cf54
SnehalChaithanya-07 <madhukarserla@gmail.com>
2026-01-15 02:43:15 +0530

    use dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,9 @@ app.get("/health", (req, res) => {
 
 app.use("/profile", MyRoute);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
