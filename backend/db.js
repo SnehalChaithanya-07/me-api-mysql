98663b559e281c1f73c1acd789ff99164dac1d7f
SnehalChaithanya-07 <madhukarserla@gmail.com>
2026-01-15 02:54:01 +0530

    connect backend to cloud mysql using env variables

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "teju2004",
-  database: "meapi"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect(err => {
-  if (err) throw err;
+  if (err) {
+    console.error("DB connection failed:", err);
+    return;
+  }
   console.log("MySQL Connected");
 });
 
